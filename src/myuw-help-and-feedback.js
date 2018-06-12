6d6ba09624f98ff23356d2220801004ad1cb9494
David Witter <david.witter@wisc.edu>
2018-06-12 12:36:33 -0500

    make a button with event

--- src/myuw-help-and-feedback.js
+++ src/myuw-help-and-feedback.js
@@ -16,10 +16,62 @@ class MyuwHelpAndFeedback extends PolymerElement {
   static get template() {
     return html`  
       <style>
-        
+        .myuw-icon-button {
+          margin: 0;
+          display: flex;
+          position: relative;
+          cursor: pointer;          
+          min-height: 36px;
+          line-height: 38px;
+          vertical-align: middle;
+          -webkit-box-align: center;
+          -webkit-align-items: center;
+          -ms-grid-row-align: center;
+          align-items: center;
+          justify-content: center;
+          border-radius: 3px;
+          box-sizing: border-box;          
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+          border: 0;          
+          padding: 0 6px;
+          background: transparent;
+          color: inherit;
+          white-space: nowrap;
+          font-weight: 500;          
+          font-size: 14px;
+          font-style: inherit;
+          font-variant: inherit;
+          font-family: inherit;
+          text-decoration: none;          
+          overflow: hidden;
+          -webkit-transition: box-shadow .4s cubic-bezier(.25,.8,.25,1),background-color .4s cubic-bezier(.25,.8,.25,1);
+          transition: box-shadow .4s cubic-bezier(.25,.8,.25,1),background-color .4s cubic-bezier(.25,.8,.25,1);
+          height: 38px;
+          min-width: 38px;          
+          border-radius: 50%;
+        }
+        .myuw-icon-button:hover {
+          background-color: rgba(0,0,0,0.1);
+        }
+        .myuw-icon-button:focus {
+          outline: none;
+        }
+        .myuw-icon-button .material-icons {
+          @apply --myuw-material-icons;
+        }
       </style>
-      <div>H AND F</div>
+      <button class="myuw-icon-button" on-click="showModal">
+        <i class="material-icons">
+          help
+        </i>
+      </button>
     `;
+  }
+  showModal() {
+    window.alert('Show the modal');
   }
   static get properties() {
     return {
--- src/shared-styles.js
+++ src/shared-styles.js
@@ -7,7 +7,32 @@ console.log('script executed');
 
 $_documentContainer.innerHTML = `<custom-style>
     <style is="myuw-custom-style">
-    
+      html {
+        --myuw-material-icons: {
+          font-family: 'Material Icons';
+          font-weight: normal;
+          font-style: normal;
+          font-size: 28px;
+          display: inline-block;
+          line-height: 1;
+          text-transform: none;
+          letter-spacing: normal;
+          word-wrap: normal;
+          white-space: nowrap;
+          direction: ltr;
+        
+          /* Support for all WebKit browsers. */
+          -webkit-font-smoothing: antialiased;
+          /* Support for Safari and Chrome. */
+          text-rendering: optimizeLegibility;
+        
+          /* Support for Firefox. */
+          -moz-osx-font-smoothing: grayscale;
+        
+          /* Support for IE. */
+          font-feature-settings: 'liga';
+        }
+      }
     </style>
   </custom-style>`;
   
